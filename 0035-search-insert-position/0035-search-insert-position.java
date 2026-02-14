a03c46e34221af08b534da9d49c7b52e89b02364
Bibek samal <bibekbibek966@gmail.com>
2026-02-14 11:51:52 +0530

    Time: 0 ms (100%), Space: 44.8 MB (45.93%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // find the key if present
+        
+        int si = 0; 
+        int ei = nums.length-1;
+        while(si<=ei){
+            int mid = (si + ei) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(target > nums[mid]){
+                si = mid + 1;
+            }else{
+                ei = mid-1;
+            }
+        }
+        return si;
+    }
+}
