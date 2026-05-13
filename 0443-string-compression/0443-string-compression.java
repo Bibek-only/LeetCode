d2ed0c32f6ecc70f97834298647ea7ce15eadd58
Bibek samal <bibekbibek966@gmail.com>
2026-05-13 11:50:34 +0530

    Time: 1 ms (99.96%), Space: 45.5 MB (48.53%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,28 +1,35 @@
 class Solution {
     public int compress(char[] chars) {
+
         int len = chars.length;
-        int newLen = 0;
+        int newLen = 0; // track the new modifyed char array
+
         for(int i = 0; i<len; i++){
-            char currentChar = chars[i];
+
+            char currentChar = chars[i]; // to tract the occurance of current consecutive charecter
             int currentCharCount = 0;
-            while(i<len && currentChar == chars[i]){
+
+            while(i<len && currentChar == chars[i]){ //coutnt logic
                 currentCharCount++;
                 i++;
             }
-            if(currentCharCount > 1){
-                //convery the current count to stirng then store each charecter to the array
+
+            if(currentCharCount > 1){ // handel if theris more than 1 consicutive charecter
                 chars[newLen++] = currentChar;
+
+                //convertthe current count to stirng then store each charecter to the array
                 String stringValue = String.valueOf(currentCharCount);
-                for (char ch: stringValue.toCharArray()){
+
+                for (char ch: stringValue.toCharArray()){ // strore each current cout digit to the char array
                 chars[newLen++] = ch;
 
                 }
                 
             }else{
-                chars[newLen] = currentChar;
+                chars[newLen] = currentChar; //handel if ther is only one chaercter
                 newLen++;
             }
-            i--;
+            i--; // cancle/neutralize the miultiple updation in while and for loop
         }
         return newLen;
     }
