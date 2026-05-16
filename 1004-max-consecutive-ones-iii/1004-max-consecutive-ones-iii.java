4cd3cb50e1ab8518c99f29a5d85463c90a0ae959
Bibek samal <bibekbibek966@gmail.com>
2026-05-16 15:16:41 +0530

    Time: 4 ms (47.68%), Space: 52.3 MB (35.52%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int len = nums.length;
-        int maxConOnes = 0;
-        int currentZeroCount = 0;
-        int currentLen = 0;
+        int left = 0; 
+        int right = 0;
+        int maxLen = 0;
+        int count = 0;
 
-        for (int i = 0; i<len; i++){ // generate all subarrays having maximum 2 zero and chek the length for updatation
-            currentZeroCount = 0;
-            for (int j = i; j<len; j++){
-                if(nums[j] == 0){
-                    currentZeroCount++;
-                }
-                if(currentZeroCount <= k){
-                    currentLen = (j - i) + 1;
-                    maxConOnes = Math.max(maxConOnes, currentLen);
-                }else{
-                    break;
-                }
+        while(right < len){
+            if(nums[right] == 0){
+                count++;
             }
+            if(count > k){
+                while(nums[left] != 0){
+                    left++;
+                }
+                left++;
+                count--;
+            }
+            int currentLen = right - left + 1;
+            maxLen = Math.max(currentLen, maxLen);
+            right++;
         }
-        return maxConOnes;
+
+        return maxLen;
     }
 }
