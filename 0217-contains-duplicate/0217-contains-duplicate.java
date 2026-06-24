0c459162362348beb30458d7a06fada7158c7fe7
Bibek samal <bibekbibek966@gmail.com>
2026-06-24 14:39:53 +0530

    Time: 16 ms (42.04%), Space: 96.9 MB (14.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int len = nums.length;
-        Arrays.sort(nums);
 
-        for(int i = 1; i<len; i++){
-            if(nums[i] == nums[i-1]){
+        int len = nums.length;
+        HashMap<Integer, Boolean> hm = new HashMap<>();
+
+        for(int i = 0; i<len; i++){
+            // check it is already exist or not
+            if(hm.containsKey(nums[i])){
                 return true;
+            }else{
+                hm.put(nums[i],true);
             }
         }
+
         return false;
+
     }
 }
