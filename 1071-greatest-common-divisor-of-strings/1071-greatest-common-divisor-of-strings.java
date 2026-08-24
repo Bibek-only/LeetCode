242d1303b5914d08b76819aa7b1a75202b61d3bc
Bibek samal <bibekbibek966@gmail.com>
2026-08-24 23:49:01 +0530

    Time: 1 ms (94.84%), Space: 43.8 MB (26.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,22 +1,22 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-
         String str3 = str1 + str2;
         String str4 = str2 + str1;
 
-        if(!str3.equals(str4)){ // if the adding the strngs->  str1+str2 and str2+str1 is identical means they contain a common sub stirng that devide them, if not then it does not contain a common substing that devide the stings
+        if( ! (str3.equals(str4))){
             return "";
         }else{
-            int str1Len = str1.length();
-            int str2Len = str2.length();
+            int len1 = str1.length();
+            int len2 = str2.length();
 
-            // now find the gretest comon divisor 
-            int gcd = Math.min(str1Len, str2Len); // now this common devisor will the length of the sub sting that devide both the strigh
-            while (!(str1Len % gcd == 0 && str2Len % gcd == 0)){
-                gcd--;
+            while(len2 != 0){
+                int r = len1 % len2;
+                len1 = len2;
+                len2 = r;
             }
 
-            return str1.substring(0,gcd); 
+            return str1.substring(0,len1);
+
         }
     }
 }
